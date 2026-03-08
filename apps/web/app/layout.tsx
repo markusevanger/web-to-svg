5537467c722e874bab9fb286820b6db7596c3dac
markus <markus@kult.design>
2026-03-08 16:44:19 +0100

    Add umami tracking

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,41 +1,37 @@
-import './globals.css'
+import "./globals.css";
 
-import {SpeedInsights} from '@vercel/speed-insights/next'
-import type {Metadata} from 'next'
-import {Spline_Sans_Mono, Tiro_Devanagari_Sanskrit} from 'next/font/google'
-import {draftMode} from 'next/headers'
-import {toPlainText} from 'next-sanity'
-import {VisualEditing} from 'next-sanity/visual-editing'
-import {Toaster} from 'sonner'
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import type { Metadata } from "next";
+import { Spline_Sans_Mono, Tiro_Devanagari_Sanskrit } from "next/font/google";
+import { draftMode } from "next/headers";
+import { toPlainText } from "next-sanity";
+import { VisualEditing } from "next-sanity/visual-editing";
+import { Toaster } from "sonner";
 
-import DraftModeToast from '@/app/components/DraftModeToast'
-import Footer from '@/app/components/Footer'
-import Header from '@/app/components/Header'
-import * as demo from '@/sanity/lib/demo'
-import {sanityFetch, SanityLive} from '@/sanity/lib/live'
-import {settingsQuery} from '@/sanity/lib/queries'
-import {resolveOpenGraphImage} from '@/sanity/lib/utils'
-import {handleError} from '@/app/client-utils'
+import DraftModeToast from "@/app/components/DraftModeToast";
+import Footer from "@/app/components/Footer";
+import Header from "@/app/components/Header";
+import * as demo from "@/sanity/lib/demo";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { settingsQuery } from "@/sanity/lib/queries";
+import { resolveOpenGraphImage } from "@/sanity/lib/utils";
+import { handleError } from "@/app/client-utils";
+import Script from "next/script";
 
-/**
- * Generate metadata for the page.
- * Learn more: https://nextjs.org/docs/app/api-reference/functions/generate-metadata#generatemetadata-function
- */
 export async function generateMetadata(): Promise<Metadata> {
-  const {data: settings} = await sanityFetch({
+  const { data: settings } = await sanityFetch({
     query: settingsQuery,
-    // Metadata should never contain stega
     stega: false,
-  })
-  const title = settings?.title || demo.title
-  const description = settings?.description || demo.description
+  });
+  const title = settings?.title || demo.title;
+  const description = settings?.description || demo.description;
 
-  const ogImage = resolveOpenGraphImage(settings?.ogImage)
-  let metadataBase: URL | undefined = undefined
+  const ogImage = resolveOpenGraphImage(settings?.ogImage);
+  let metadataBase: URL | undefined = undefined;
   try {
     metadataBase = settings?.ogImage?.metadataBase
       ? new URL(settings.ogImage.metadataBase)
-      : undefined
+      : undefined;
   } catch {
     // ignore
   }
@@ -49,48 +45,57 @@ export async function generateMetadata(): Promise<Metadata> {
     openGraph: {
       images: ogImage ? [ogImage] : [],
     },
-  }
+  };
 }
 
 const splineSansMono = Spline_Sans_Mono({
-  variable: '--font-spline-sans-mono',
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  display: 'swap',
-})
+  variable: "--font-spline-sans-mono",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+});
 
 const tiro = Tiro_Devanagari_Sanskrit({
-  variable: '--font-tiro',
-  weight: ['400'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  display: 'swap',
-})
+  variable: "--font-tiro",
+  weight: ["400"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
-export default async function RootLayout({children}: {children: React.ReactNode}) {
-  const {isEnabled: isDraftMode} = await draftMode()
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { isEnabled: isDraftMode } = await draftMode();
 
   return (
-    <html lang="en" className={`${splineSansMono.variable} ${tiro.variable} bg-white text-black scroll-smooth`}>
+    <html
+      lang="en"
+      className={`${splineSansMono.variable} ${tiro.variable} bg-white text-black scroll-smooth`}
+    >
       <body className="bg-dots">
         <div className="min-h-screen pt-24">
-          {/* The <Toaster> component is responsible for rendering toast notifications used in /app/client-utils.ts and /app/components/DraftModeToast.tsx */}
           <Toaster />
           {isDraftMode && (
             <>
               <DraftModeToast />
-              {/*  Enable Visual Editing, only to be rendered when Draft Mode is enabled */}
               <VisualEditing />
             </>
           )}
-          {/* The <SanityLive> component is responsible for making all sanityFetch calls in your application live, so should always be rendered. */}
           <SanityLive onError={handleError} />
           <Header />
           <main className="">{children}</main>
           <Footer />
         </div>
         <SpeedInsights />
+        <Script
+          defer
+          src="https://analytics.markusevanger.no/script.js"
+          data-website-id="e30e9d83-d621-4487-a4b0-9657080978fc"
+        />
       </body>
     </html>
-  )
+  );
 }
